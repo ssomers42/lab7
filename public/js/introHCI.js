c8403ecb3dd86558b4fbccf757621589a5e40c69
Stephan <ssomers42@gmail.com>
2018-02-22 01:16:18 -0800

    Lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(like)
 	// your code here
+}
+
+function like(e) {
+	e.preventDefault();
+	ga("send", "event", "like", "click");
 }
